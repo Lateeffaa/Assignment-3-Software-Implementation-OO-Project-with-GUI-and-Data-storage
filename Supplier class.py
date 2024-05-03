d6775788a5e93b8dd8538e1fa280dfe4483c046c
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 16:46:16 +0400

    Update Supplier class.py

--- Supplier class.py
+++ Supplier class.py
@@ -1,11 +1,9 @@
 class Supplier:
     """Class to represent the supplier information"""
-    def __init__(self, supplierName, supplierID, supplierAddress, email, phoneNum, supplierCompany, minGuests, maxGuests):
+    def __init__(self, supplierName, supplierID, supplierAddress, email, phoneNum, supplierCompany):
         self.supplierName = supplierName
-        self.supplierID = int(supplierID)
+        self.supplierID = supplierID
         self.supplierAddress = supplierAddress
         self.email = email
-        self.phoneNum = int(phoneNum)
+        self.phoneNum = phoneNum
         self.supplierCompany = supplierCompany
-        self.minGuests = int(minGuests)
-        self.maxGuests = int(maxGuests)
