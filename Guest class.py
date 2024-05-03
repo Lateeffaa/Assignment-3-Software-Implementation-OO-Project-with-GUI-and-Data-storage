0ce4e687198477df6708db1da527720f81210d4d
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 16:47:39 +0400

    Update Guest class.py

--- Guest class.py
+++ Guest class.py
@@ -2,7 +2,7 @@ class Guest:
     """Class to represent the guest information"""
     def __init__(self, guestName, guestID, guestAddress, email, phoneNum):
         self.guestName = guestName
-        self.guestID = int(guestID)
+        self.guestID = guestID
         self.guestAddress = guestAddress
         self.email = email
-        self.phoneNum = int(phoneNum)
+        self.phoneNum = phoneNum
