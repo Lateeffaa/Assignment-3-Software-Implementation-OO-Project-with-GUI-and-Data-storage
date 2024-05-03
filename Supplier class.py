19750e6d7cd22fde2ffbe0c369b4bd0fff03ff6e
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 19:56:16 +0400

    Delete Supplier class.py

--- Supplier class.py
+++
@@ -1,8 +0,0 @@
-class Supplier:
-    """Class to represent the supplier information"""
-    def __init__(self, supplierName, supplierID, supplierAddress, email, phoneNum):
-        self.supplierName = supplierName
-        self.supplierID = supplierID
-        self.supplierAddress = supplierAddress
-        self.email = email
-        self.phoneNum = phoneNum
