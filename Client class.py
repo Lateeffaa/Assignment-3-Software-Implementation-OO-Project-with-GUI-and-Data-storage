54eaacbc9ced066447a7f111c1644cb1ad6c9804
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 19:55:28 +0400

    Delete Client class.py

--- Client class.py
+++
@@ -1,9 +0,0 @@
-class Client:
-    """Class to represent the client information"""
-    def __init__(self, clientName, clientID, clientAddress, email, phoneNum, clientBudget):
-        self.clientName = clientName
-        self.clientID = clientID
-        self.clientAddress = clientAddress
-        self.email = email
-        self.phoneNum = phoneNum
-        self.clientBudget = clientBudget
