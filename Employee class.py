74fc53258a2ad971d5a2f3fad2578554956c6044
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 12:07:28 +0400

    Update Employee class.py

--- Employee class.py
+++ Employee class.py
@@ -1,11 +1,11 @@
 class Employee:
-    """Class to represent the employee details"""
+    """Class to represent the employee information"""
     def __init__(self, name, empID, department, jobTitle, basicSalary, age, dateOfBirth, passportID, passportExpiry, managerID):
         self.name = name
         self.empID = int(empID)
         self.department = department
         self.jobTitle = jobTitle
-        self.basicSalary = basicSalary
+        self.basicSalary = float(basicSalary)
         self.age = int(age)
         self.dateOfBirth = dateOfBirth
         self.passportID = passportID
