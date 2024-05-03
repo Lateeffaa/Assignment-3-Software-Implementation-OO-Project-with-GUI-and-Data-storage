51d7c3d917d4736932f2db6b587bbf57157da425
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 19:56:07 +0400

    Delete Guest class.py

--- Guest class.py
+++
@@ -1,8 +0,0 @@
-class Guest:
-    """Class to represent the guest information"""
-    def __init__(self, guestName, guestID, guestAddress, email, phoneNum):
-        self.guestName = guestName
-        self.guestID = guestID
-        self.guestAddress = guestAddress
-        self.email = email
-        self.phoneNum = phoneNum
