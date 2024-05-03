bee0f7423018f64e938351a35afa0aeb2a392a61
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 12:07:45 +0400

    Update Client class.py

--- Client class.py
+++ Client class.py
@@ -1,2 +1,9 @@
 class Client:
-  
+    """Class to represent the client information"""
+    def __init__(self, clientName, clientID, clientAddress, email, phoneNum, clientBudget):
+        self.clientName = clientName
+        self.clientID = int(clientID)
+        self.clientAddress = clientAddress
+        self.email = email
+        self.phoneNum = int(phoneNum)
+        self.clientBudget = float(clientBudget)
