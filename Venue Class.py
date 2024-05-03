2217993e76b79cbe9a7e15bb77fb5f691f79927f
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-05-03 19:56:25 +0400

    Delete Venue Class.py

--- Venue Class.py
+++
@@ -1,9 +0,0 @@
-class Venue:
-    """Class to represent the venue information"""
-    def __init__(self, venueName, venueID, venueAddress, contactNum, minGuests, maxGuests):
-        self.venueName = venueName
-        self.venueID = venueID
-        self.venueAddress = venueAddress
-        self.contactNum = contactNum
-        self.minGuests = minGuests
-        self.maxGuests = maxGuests
